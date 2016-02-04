51df7054c086913b46854eff3e7c665feb850a1f
Konstantin Shpolvind <skv-pfc@eml.ru>
2016-02-05 00:41:02 +0600

    Layout xml intro

---
+++ clean_shit.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm -rf app/code/local/Tutorial
+rm -rf app/design/frontend/base/default/layout/Tutorial_*
+rm -rf app/design/frontend/base/default/template/Tutorial_*
+rm -rf app/etc/modules/Tutorial_*.xml
--- devel/04.03.OverrideBlockClass/app/code/local/Tutorial/L4E2Echoparameters/Block/Page/Html/Breadcrumbs.php
+++ devel/04.03.OverrideBlockClass/app/code/local/Tutorial/L4E2Echoparameters/Block/Page/Html/Breadcrumbs.php
@@ -3,19 +3,23 @@
 
 class Tutorial_L4E2Echoparameters_Block_Page_Html_Breadcrumbs extends Mage_Page_Block_Html_Breadcrumbs
 {
+
     public function __construct()
+//    public function _construct()
     {
         parent::__construct();
-        $this->addCrumb('GOOGLE', 'Google');
+        $this->addCrumb(
+            'GOOGLE',
+            ['label'=>'Doogle', 'title'=>'Woogle', 'link'=>'http://google.com',]
+//            ['label'=>'Doogle', 'title'=>'Woogle', 'link'=>'http://google.com', 'first'=>true, ]
+        );
     }
-
+/*
     protected function _toHtml()
     {
-//        $localHtml = "<h3>Duck a Tuck with a Christmas Tree!!!</h3>";
         $parentHtml = parent::_toHtml();
-//
-//        $htmlContent = $localHtml. $parentHtml;
         return $parentHtml;
     }
+*/
 
 }
